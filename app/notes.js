1169e973d8aca2eda05faf379532530f71e5fc8e
VadimShmakov <shmakovvadim@mail.ru>
2022-01-23 11:48:34 +0500

    [note] Removed unused

--- app/notes.js
+++ app/notes.js
@@ -7,29 +7,8 @@ const Notes = function (selector, tuner) {
     this.$notesMap = {}
 }
 
-Notes.prototype.active = function ($note) {
-    this.clearActive()
-    $note.classList.add('active')
-    this.$notesList.scrollLeft =
-        $note.offsetLeft - (this.$notesList.clientWidth - $note.clientWidth) / 2
-}
-
-Notes.prototype.clearActive = function () {
-    const $active = this.$notesList.querySelector('.active')
-    if ($active) {
-        $active.classList.remove('active')
-    }
-}
-
 Notes.prototype.update = function (note) {
     this.$frequency.childNodes[0].textContent = parseFloat(
         note.frequency
     ).toFixed(1)
-}
-
-Notes.prototype.toggleAutoMode = function () {
-    if (this.isAutoMode) {
-        this.clearActive()
-    }
-    this.isAutoMode = !this.isAutoMode
 }
